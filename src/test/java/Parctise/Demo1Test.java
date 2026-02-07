43ca1c76cef1b3bdcae3ebfa9c3a966bbe6dfff7
Dinesh <dineshvasan990915@gmail.com>
2026-02-07 08:42:22 +0530

    The first time committed

---
+++ src/test/java/Parctise/Demo1Test.java
@@ -0,0 +1,12 @@
+package Parctise;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	@Test(groups = "smoke")
+	public void fruitstest() {
+		System.out.println("Apple");
+	}
+
+}
---
+++ src/test/java/Parctise/DemoTest.java
@@ -0,0 +1,12 @@
+package Parctise;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test(groups = "regression")
+	public void colourtest() {
+		System.out.println("Black");
+	}
+
+}
